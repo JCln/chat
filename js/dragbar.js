02cab077ab4e33aca7cdeae739dd215e9fe6adfe
JCln <ghmantera@gmail.com>
2023-02-21 17:19:45 +0330

    second commit Ya Ali

---
+++ js/dragbar.js
@@ -0,0 +1,16 @@
+const left = document.getElementById('drag-left');
+const right = document.getElementById('drag-right');
+const bar = document.getElementById('dragbar');
+
+const drag = (e) => {
+    document.selection ? document.selection.empty() : window.getSelection().removeAllRanges();
+    left.style.width = (e.pageX - bar.offsetWidth / 2) + 'px';
+};
+
+bar.addEventListener('mousedown', () => {
+    document.addEventListener('mousemove', drag);
+});
+
+bar.addEventListener('mouseup', () => {
+    document.removeEventListener('mousemove', drag);
+});
