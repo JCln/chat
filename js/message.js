822b8cf9ae18b3a07987c6d8a90a0dbff1e3f463
JCln <ghmantera@gmail.com>
2023-02-26 16:06:10 +0330

    with image

--- js/message.js
+++ js/message.js
@@ -1,11 +1,11 @@
 "use strict";
 
 let message = [
-    { text: 'Lorem ipsum dolor sit amet.', isMyMessage: false, className: 'leftish', imgUrl: '../assets/imgs/marker_red.png' },
-    { text: 'Lorem ipsum dolor sit amet.', isMyMessage: true, className: 'rightish', imgUrl: '../assets/imgs/marker-icon.png' },
-    { text: 'Lorem ipsum dolor', isMyMessage: true, className: 'rightish', imgUrl: '../assets/imgs/marker-icon.png' },
-    { text: 'Lorem ipsum dolor sit amet.', isMyMessage: false, className: 'leftish', imgUrl: '../assets/imgs/marker_red.png' },
-    { text: 'Lorem ipsum dolor', isMyMessage: true, className: 'rightish', imgUrl: '../assets/imgs/marker-icon.png' },
+    { text: 'test', isImageFile: true, imageFile: '../assets/imgs/rr2.JPG', isMyMessage: false, className: 'leftish', imgUrl: '../assets/imgs/marker_red.png' },
+    { text: 'Lorem ipsum dolor sit amet.', isImageFile: false, isMyMessage: true, className: 'rightish', imgUrl: '../assets/imgs/marker-icon.png' },
+    { text: 'Lorem ipsum dolor', isImageFile: false, isMyMessage: true, className: 'rightish', imgUrl: '../assets/imgs/marker-icon.png' },
+    { text: 'Lorem ipsum dolor sit amet.', isImageFile: false, isMyMessage: false, className: 'leftish', imgUrl: '../assets/imgs/marker_red.png' },
+    { text: 'Lorem ipsum dolor', isImageFile: false, isMyMessage: true, className: 'rightish', imgUrl: '../assets/imgs/marker-icon.png' },
 ];
 
 function isNull(e) {
@@ -17,19 +17,50 @@ function emptyUserInput() {
     const userInput = document.getElementById('user-input');
     userInput.value = '';
 }
+function scrollToBottomOfPage() {
+    window.scrollTo(0, document.body.scrollHeight);
+}
+function showImageMessage() {
+    data += `<div class="${message[index].className}" 
+    ${document.styleSheets[0].addRule(`${'.message-body .' + message[index].className}::before`, 'content: url("' + message[index].imgUrl + '");')}       
+    >`
+
+    data += `<img src="${message[index].imageFile}" alt="test">`
+    data += '</div>'
+}
+function showTextMessage() {
+    data += `<div class="${message[index].className}" 
+        ${document.styleSheets[0].addRule(`${'.message-body .' + message[index].className}::before`, 'content: url("' + message[index].imgUrl + '");')}       
+        >`
+    data += `${message[index].text}`
+    data += '</div>'
+}
 function getLatestData() {
     let messageElement = document.getElementById('messages');
     let data = "";
     for (let index = 0; index < message.length; index++) {
-        data += `<div class="${message[index].className}" 
+        if (message[index].isImageFile) {
+            // showImageMessage();
+            data += `<div class="${message[index].className}" 
+            ${document.styleSheets[0].addRule(`${'.message-body .' + message[index].className}::before`, 'content: url("' + message[index].imgUrl + '");')}       
+            >`
+
+            data += `<img src="${message[index].imageFile}" alt="test">`
+            data += '</div>'
+        }
+        else {
+            data += `<div class="${message[index].className}" 
         ${document.styleSheets[0].addRule(`${'.message-body .' + message[index].className}::before`, 'content: url("' + message[index].imgUrl + '");')}       
         >`
-        data += `${message[index].text}`
-        data += '</div>'
+            data += `${message[index].text}`
+            data += '</div>'
+        }
+
     }
+
     messageElement.innerHTML += data;
     emptyUserInput();
-
+    scrollToBottomOfPage();
 }
 function addMySingleMessage(a) {
     message.push({ text: a, isMyMessage: true, className: 'rightish', imgUrl: '../assets/imgs/marker-icon.png' });
@@ -49,6 +80,14 @@ function messageEnter() {
     if (userInput != null && isNull(userInput.value)) {
         addMySingleMessage(userInput.value);
     }
+}
+function uploadFile() {
+    // let anyFile = document.getElementById("user-input").files[0];
+    // let formData = new FormData();
+
+    // formData.append("anyFile", anyFile);
+    // fetch('/upload/image', { method: "POST", body: formData });
+
 }
 // latest messages AJAX CALL if zero, error happens
 setTimeout(() => {
